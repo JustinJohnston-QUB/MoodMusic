94267cf4099db92f66242f490eb37eebdbad2fcd
Justin <Justin@192.168.1.14>
2019-02-27 22:00:24 +0000

    updated all pages to add mood into the database

--- src/views/Webapp.java
+++ src/views/Webapp.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.h2.mvstore.MVMap;
 
 import model.Artist;
+import model.Mood;
 import model.Song;
 import model.Product;
 import storage.DatabaseInterface;
@@ -110,6 +111,9 @@ public class Webapp extends DynamicWebPage
 			MVMap<String, Artist> artists= db.s.openMap("Artist");
 			List<String> artistkeys = artists.keyList() ;
 			Artist iartist = new Artist();
+			MVMap<String, Mood> moods= db.s.openMap("Mood");
+			List<String> moodkeys = moods.keyList() ;
+			Mood imood = new Mood();
 			String stringToSendToWebBrowser = PageElements.header() +
 					"<body>\n" + PageElements.Navbar()+ PageElements.Search()+
 
@@ -117,11 +121,11 @@ public class Webapp extends DynamicWebPage
 
 					"<div class=\"container\">\n" + 
 					"  <div class=\"row\">\n" + 
-					"    <div class=\"col s4\">\n" + 
+					"    <div class=\"col s6\">\n" + 
 					"		<h2>Add song</h2>\n" + 
 					"		<form action=\"../addsong.html\" method = \"GET\"id = \"addsong\">\n " + 
 					"  			Song Title " + 
-					"  			<input type=\"text\" name=\"songtitle\" value=\"title\">\n" + 
+					"  			<input type=\"text\" name=\"songtitle\" placeholder=\"title\">\n" + 
 					"  			<br>\n" + 
 					" 			song Length <input type=\"time\" name=\"songlength\" value=\"time\">\n" + 
 					"  			<br>\n" + 
@@ -130,10 +134,23 @@ public class Webapp extends DynamicWebPage
 					"			<div class=\"input-field\"> "+
 					"			Artist Name<select name=\"Artist\" class = \"browser-default\" form=\"addsong\">\n"+
 					"			option value=\"\" disabled selected>Choose your option</option>" ;
+			
 			for(int i = 0; i < artistkeys.size();i++) {
 				String artistUniqueID = artistkeys.get(i);
 				iartist = artists.get(artistUniqueID);
 				stringToSendToWebBrowser += "  <option value=\""+iartist.uniqueID +"\">"+iartist.artistName+"</option>\n";
+			}
+			
+			
+			stringToSendToWebBrowser +=			 "</select>" + 
+												"  </div>"+
+												"			<div class=\"input-field\"> "+
+												"			Mood<select name=\"mood\" class = \"browser-default\" form=\"addsong\">\n"+
+												"			option value=\"\" disabled selected>Choose your option</option>" ;
+			for(int i = 0; i < moodkeys.size();i++) {
+				String moodUniqueID = moodkeys.get(i);
+				imood= moods.get(moodUniqueID);
+				stringToSendToWebBrowser += "  <option value=\""+imood.moodname +"\">"+imood.moodname+"</option>\n";
 			}
 
 			stringToSendToWebBrowser += "</select>" + 
@@ -142,7 +159,7 @@ public class Webapp extends DynamicWebPage
 					"  <input type=\"submit\" value=\"Submit\">\n" + 
 					"</form> \n" + 
 					"    </div>\n" + 
-					"    <div class=\"col s8\">\n" + 
+					"    <div class=\"col s6 m0\">\n" + 
 					"      <h3>We are always looking for more artists</h3> \n" + 
 					"      <p style = lead>Add an artist on this page</p>\n" + 
 					"      <bt><p>more text...</p>\n" + 
@@ -197,8 +214,12 @@ public class Webapp extends DynamicWebPage
 
 			//Page which adds songs to the database        	
 		}else if(toProcess.path.equalsIgnoreCase("addsong.html")) {
+			MVMap<String, Mood> moods= db.s.openMap("Mood");
+			List<String> moodkeys = moods.keyList() ;
+			Mood imood = new Mood();
 			MVMap<String, Artist> artists= db.s.openMap("Artist");
 			Artist art1 = artists.get(toProcess.params.get("Artist"));
+			Mood mood1 = moods.get(toProcess.params.get("mood"));
 			Song isong = new Song();
 			isong.uniqueID = "song_"+System.currentTimeMillis();
 			isong.songtitle= toProcess.params.get("songtitle");	
@@ -211,9 +232,15 @@ public class Webapp extends DynamicWebPage
 				art1.artistSongs = new ArrayList<String>();
 				art1.artistSongs.add(isong.uniqueID);
 			}
-
+			if(mood1.songID !=null) {
+				mood1.songID.add(isong.uniqueID);
+			}else {
+				mood1.songID  = new ArrayList<String>();
+				mood1.songID .add(isong.uniqueID);
+			}
 			songs.put(isong.uniqueID, isong);
 			artists.put(art1.uniqueID, art1);
+			moods.put(mood1.moodname, mood1);
 			db.commit();
 			String stringToSendToWebBrowser = PageElements.header() + PageElements.Navbar()+ PageElements.Search()+
 					"<body>\n" + 
@@ -462,25 +489,46 @@ public class Webapp extends DynamicWebPage
 			}		
 
 			// end of artist loop 
+			//start of mood loop   
+			MVMap<String, Mood> moods= db.s.openMap("Mood");
+			List<String> moodkeys = moods.keyList() ;
+			Mood imood = new Mood();
+			Mood tempmood = new Mood();
+			String moodresult = "";
+			for(int k = 0; k < moodkeys.size();k++) {	
+				String moodUniqueID = moodkeys.get(k);
+				tempmood = moods.get(moodUniqueID);
+				if(tempmood.songID != null) {
+					for(int j = 0; j < tempmood.songID.size() ;j++) {
+						String moodname = tempmood.songID.get(j);
+						if(moodname.equalsIgnoreCase(isong.uniqueID.toLowerCase())) {
+							imood = moods.get(moodUniqueID);
+						}
+					}
+				}
+			}		
+
+			// end of mood loop 
 			String stringToSendToWebBrowser = PageElements.header() + PageElements.Navbar()+ PageElements.Search()+
 					"  <div class=\" col s12 white\">\n" + 
 					"    <div class=\"container-fluid\">\n" + 
 					"      <div class=\"row\">\n" + 
-					"        <div class=\"col s12 white\">\n" + 
+					"        <div class=\"col s10 offset-s1 white\">\n" + 
 					"          <div class=\"row\">\n" + 
 					"            <div class=\"col s2\"></div>\n" + 
-					"            <div class=\"col s12 \">\n" + 
-					"              <h1>"+ isong.songtitle +"</h1>\n" + 
-					"					<a href = \"../artistpage.html?artist="+iartist.uniqueID+"\"><h3>"+"      Artist: "+iartist.artistName+"</h3></a>\n"+
+					"            <div class=\"col s12\">\n" + 
+					"              <h1>"+isong.songtitle +"</h1>\n" + 
+					"					<a href = \"../artistpage.html?artist="+iartist.uniqueID+"\"><h3>"+"Artist: "+iartist.artistName+"</h3></a>\n"+
 					"            </div>\n" + 
 					"          </div>\n" + 
-					"          <div class=\"row\">\n" + 
-					"            <div class=\"col s2\" style=\"\"><img class=\"circle responsive-img\" src=\"https://static.pingendo.com/img-placeholder-3.svg\" width=\"300\" height=\"300\"></div>\n" + 
+					"          <div class=\"row offset-s1\">\n" + 
+					"            <div class=\"col s2 \" style=\"\"><img class=\"circle responsive-img\" src=\"https://static.pingendo.com/img-placeholder-3.svg\" width=\"200px\" height=\"200px\"></div>\n" + 
 					"            <div class=\"col s10\">\n" + 
 					"              <div class=\"row\" style=\"	min-height: 200px;\">\n" + 
 					"                <div class=\"col s12\" style=\"\">\n" + 
 					"                  <p class=\"lead\" style=\"\">" + "song length: " + isong.songlength +"<br></p>\n" + 
 					"                  <p class=\"lead\" style=\"\">" + "song link: " + isong.songLink +"<br></p>\n" + 
+					"                  <p class=\"lead\" style=\"\">" + "song mood: " + imood.moodname +"<br></p>\n" + 
 					"                </div>\n" + 
 					"              </div>\n" + 
 					"					</div>\n" + 
@@ -574,6 +622,49 @@ public class Webapp extends DynamicWebPage
 							"</html>";
 			toProcess.r = new WebResponse( WebResponse.HTTP_OK, WebResponse.MIME_HTML, stringToSendToWebBrowser );
 			return true;
+		}else if(toProcess.path.equalsIgnoreCase("mood.html")){
+			String stringToSendToWebBrowser = "<!DOCTYPE html>\n" + 
+					PageElements.header()+ "<body>\n"  + PageElements.Navbar()+ PageElements.Search()+
+					"\n" + 
+					"<div class=\"container\">\n" + 
+					"  <div class=\"row\">\n" + 
+					"    <div class=\"col s12\">\n" + 
+					"			<h2>Add Mood</h2>\n" + 
+					"			<form action=\"../addmood.html\" method = \"GET\"id = \"addmood\">\n " + 
+					" 			 Mood Name " + 
+					" 			 <input type=\"text\" name=\"moodname\" placeholder=\"Name\">\n" + 
+					" 			 <br>\n" + 
+					"  			Description<input type=\"text\" name=\"mooddescription\" placeholder=\"description\">\n" + 
+					"  			<br><br>\n" + 
+					"  			<input type=\"submit\" value=\"Submit\">\n" + 
+					"			</form> \n" + 
+					"    </div>\n" +  
+					"  </div>\n" + 
+					"</div>"+
+
+        			"\n" + 
+        			"</body>\n" + 
+        			"</html> ";
+			toProcess.r = new WebResponse( WebResponse.HTTP_OK, WebResponse.MIME_HTML, stringToSendToWebBrowser );
+			return true;      	
+		}else if(toProcess.path.equalsIgnoreCase("addmood.html")) {
+			Mood imood = new Mood();
+			imood.moodname = toProcess.params.get("moodname");	
+			imood.mooddescription = toProcess.params.get("mooddescription");	
+			MVMap<String, Mood> moods= db.s.openMap("Mood");
+			moods.put(imood.moodname, imood);
+			db.commit();
+			String stringToSendToWebBrowser = PageElements.header() + PageElements.Navbar()+ PageElements.Search()+
+					"<body>\n" + 
+					"	<h2>Mood added</h2>\n" + 
+					"</body>\n" + 
+					"</html>\n" + 
+					"";
+			toProcess.r = new WebResponse( WebResponse.HTTP_OK, WebResponse.MIME_HTML, stringToSendToWebBrowser );
+			return true;
+
+
+            		
 		}
 
 		return false;   
