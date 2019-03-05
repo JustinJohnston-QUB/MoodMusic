39f156c6e31ddbcb8a3f7006b04b2f69a946e63d
Justin <Justin@192.168.1.14>
2019-03-05 22:25:34 +0000

    fixed small formating errors in about us view

--- src/views/AboutUsView.java
+++ src/views/AboutUsView.java
@@ -57,29 +57,29 @@ public class AboutUsView extends DynamicWebPage{
 					"      <div class=\"row\">\r\n" + 
 					"        <h1 class=\"header center \"><b>Meet the Team</b></h1>\r\n" + 
 					"        <div class=\"col s12 m4\">\r\n" + 
-					"          <div class=\"icon-block\">\r\n" + 
-					"            <img src=\"images/face.jpg\" alt=\"\" class=\"circle responsive-img center-align \">\r\n" + 
+					"          <div class=\"icon-block center\">\r\n" + 
+					"            <img src=\"images/face.jpg\" alt=\"\" class=\"circle responsive-img \">\r\n" + 
 					"            <h5 class=\"center\">Johnny Hume</h5>\r\n" + 
 					"\r\n" + 
-					"            <p class=\"light\">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components. Additionally, we refined animations and transitions to provide a smoother experience for developers.</p>\r\n" + 
+					"            <p class=\"light left-align\">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components. Additionally, we refined animations and transitions to provide a smoother experience for developers.</p>\r\n" + 
 					"          </div>\r\n" + 
 					"        </div>\r\n" + 
 					"\r\n" + 
 					"        <div class=\"col s12 m4\">\r\n" + 
-					"          <div class=\"icon-block\">\r\n" + 
-					"            <img src=\"images/face.jpg\" alt=\"\" class=\"circle responsive-img center-align\">\r\n" + 
+					"          <div class=\"icon-block center\">\r\n" + 
+					"            <img src=\"images/face.jpg\" alt=\"\" class=\"circle responsive-img \">\r\n" + 
 					"            <h5 class=\"center\">Justin Johnston</h5>\r\n" + 
 					"\r\n" + 
-					"            <p class=\"light\">By utilizing elements and principles of Material Design, we were able to create a framework that incorporates components and animations that provide more feedback to users. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.</p>\r\n" + 
+					"            <p class=\"light left-align\">By utilizing elements and principles of Material Design, we were able to create a framework that incorporates components and animations that provide more feedback to users. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.</p>\r\n" + 
 					"          </div>\r\n" + 
 					"        </div>\r\n" + 
 					"\r\n" + 
 					"        <div class=\"col s12 m4\">\r\n" + 
-					"          <div class=\"icon-block\">\r\n" + 
-					"            <img src=\"images/face.jpg\" alt=\"\" class=\"circle responsive-img center-align\">\r\n" + 
+					"          <div class=\"icon-block center\">\r\n" + 
+					"            <img src=\"images/face.jpg\" alt=\"\" class=\"circle responsive-img\">\r\n" + 
 					"            <h5 class=\"center\">Karolis katauskas</h5>\r\n" + 
 					"\r\n" + 
-					"            <p class=\"light\">We have provided detailed documentation as well as specific code examples to help new users get started. We are also always open to feedback and can answer any questions a user may have about Materialize.</p>\r\n" + 
+					"            <p class=\"light left-align\">We have provided detailed documentation as well as specific code examples to help new users get started. We are also always open to feedback and can answer any questions a user may have about Materialize.</p>\r\n" + 
 					"          </div>\r\n" + 
 					"        </div>\r\n" + 
 					"      </div>\r\n" + 
