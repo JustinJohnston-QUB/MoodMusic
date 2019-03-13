44a4fdf919499a30acdb923d20306d88cb3f7c06
Justin <Justin@192.168.1.14>
2019-03-13 23:07:10 +0000

    Initial work on homepage
    2 column page which splits in the centre if the screensize gets too low
    random back ground image on one side
    Work to complete - pulling random song, mood, artist and store product.
    these will go in the right hand side cards

--- src/views/HomePage.java
+++ src/views/HomePage.java
@@ -1,5 +1,8 @@
 package views;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 import storage.DatabaseInterface;
 import storage.FileStoreInterface;
 import web.WebRequest;
@@ -16,92 +19,46 @@ public class HomePage extends DynamicWebPage
 	{
         if(toProcess.path.equalsIgnoreCase("homepage"))
         {
-        	//Lab 1 Task 4
-        	//Change this string so that it contains HTML from a page you created in Pingendo 
-        	String stringToSendToWebBrowser = "<!DOCTYPE html>\r\n" + 
-        			"<html>\r\n" + 
-        			"    <head>\r\n" + 
+          	ArrayList<String> pageimages = new ArrayList<String>();
+        	pageimages.add("images/homepage/1.gif");
+        	pageimages.add("images/homepage/2.gif");
+        	pageimages.add("images/homepage/3.gif");
+        	pageimages.add("images/homepage/4.gif");
+        	pageimages.add("images/homepage/5.gif");
+        	pageimages.add("images/homepage/6.gif");//( max - min )) + min 
+        	Random rn = new Random();
+        	int randomimage = rn.nextInt(pageimages.size());
+        	String stringToSendToWebBrowser = PageElements.header()+PageElements.Navbar()+PageElements.Search()+
+        			"    <div class=\"row\">\r\n" + 
         			"\r\n" + 
-        			"            <meta charset=\"utf-8\">\r\n" + 
-        			"            <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\r\n" + 
-        			"  			<link href=\"../css/materialize.css\" type=\"text/css\" rel=\"stylesheet\" media=\"screen,projection\"/>\r\n" + 
-        			//"            <link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css\" integrity=\"sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm\" crossorigin=\"anonymous\">\r\n" + 
+        			"      <div class=\"col s12 m6 l6\"> <!Right Hand Side Panel-->\r\n" + 
+        			"<img class=\"materialboxed\" width=\"100%\" src=\""+pageimages.get(randomimage)+"\">"+
+        			"   </div>\r\n" + 
         			"\r\n" + 
-        			"  			<link href=\"https://fonts.googleapis.com/icon?family=Material+Icons\" rel=\"stylesheet\">\r\n" + 
-        			"            <script src=\"https://code.jquery.com/jquery-3.2.1.slim.min.js\" integrity=\"sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN\" crossorigin=\"anonymous\"></script>\r\n" + 
-        			"            <script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js\" integrity=\"sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q\" crossorigin=\"anonymous\"></script>\r\n" + 
-        			//"            <script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js\" integrity=\"sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl\" crossorigin=\"anonymous\"></script>\r\n" + 
-        			"            <script>$(function () {\r\n" + 
-        			"                $('[data-toggle=\"popover\"]').popover()\r\n" + 
-        			"              })</script>\r\n" + 
+        			"      <div class=\"col s12 m6 l6\"> <!-- Left Hand Panel -->\r\n" + 
+        			"  <div class=\"carousel carousel-slider center \">\r\n" + 
+        			"    <div class=\"carousel-item orange white-text\" href=\"#one!\">\r\n" + 
+        			"      <h2>First Panel</h2>\r\n" + 
+        			"      <p class=\"white-text\">This is your first panel</p>\r\n" + 
+        			"    </div>\r\n" + 
+        			"    <div class=\"carousel-item white black-text\" href=\"#two!\">\r\n" + 
+        			"      <h2>Second Panel</h2>\r\n" + 
+        			"      <p class=\"white-text\">This is your second panel</p>\r\n" + 
+        			"    </div>\r\n" + 
+        			"    <div class=\"carousel-item orange white-text\" href=\"#three!\">\r\n" + 
+        			"      <h2>Third Panel</h2>\r\n" + 
+        			"      <p class=\"white-text\">This is your third panel</p>\r\n" + 
+        			"    </div>\r\n" + 
+        			"    <div class=\"carousel-item white black-text\" href=\"#four!\">\r\n" + 
+        			"      <h2>Fourth Panel</h2>\r\n" + 
+        			"      <p class=\"white-text\">This is your fourth panel</p>\r\n" + 
+        			"    </div>\r\n" + 
+        			"  </div>\r\n" + 
+        			"        "+
+        			"      </div>\r\n" + 
         			"\r\n" + 
-        			"        <body>\r\n" + 
-        			"\r\n" + PageElements.Navbar()+PageElements.scripts()+
-        			
-        			"\r\n" + 
-        			"                      <div class=\"row\">\r\n" + 
-        			"\r\n" + 
-        			"                        <div class=\"col-sm-4\">\r\n" + 
-        			"                          \r\n" + 
-        			"                          <div class=\"card text-center\">\r\n" + 
-        			"                            <div class=\"card-header\">\r\n" + 
-        			"                              <ul class=\"nav nav-tabs card-header-tabs\">\r\n" + 
-        			"                                <li class=\"nav-item\">\r\n" + 
-        			"                                  <a class=\"nav-link active\" href=\"#\"><span class=\"badge badge-primary\">Featured</span></a>\r\n" + 
-        			"                                </li>\r\n" + 
-        			"                                <li class=\"nav-item\">\r\n" + 
-        			"                                  <a class=\"nav-link\" href=\"#\"><span class=\"badge badge-success\">Recent</span></a>\r\n" + 
-        			"                                </li>\r\n" + 
-        			"                                <li class=\"nav-item\">\r\n" + 
-        			"                                  <a class=\"nav-link\" href=\"#\"><span class=\"badge badge-danger\">Trending</span></a>\r\n" + 
-        			"                                </li>\r\n" + 
-        			"                              </ul>\r\n" + 
-        			"                            </div>\r\n" + 
-        			"                            <div class=\"card-body\">\r\n" + 
-        			"                              <h5 class=\"card-title\">Song Name</h5>\r\n" + 
-        			"                              <p class=\"card-text\">Song Artist</p>\r\n" + 
-        			"                              <a href=\"#\" class=\"btn btn-primary\">Listen</a>\r\n" + 
-        			"                              <button type=\"button\" class=\"btn btn-sm btn-danger\" data-toggle=\"popover\" title=\"User Not Logged In\" data-content=\"You are not logged in. Please Login to add the song to your playlist.\">+</button>\r\n" + 
-        			"\r\n" + 
-        			"                              <h5 class=\"card-title\">Song Name</h5>\r\n" + 
-        			"                              <p class=\"card-text\">Song Artist</p>\r\n" + 
-        			"                              <a href=\"#\" class=\"btn btn-primary\">Listen</a>\r\n" + 
-        			"                              <button type=\"button\" class=\"btn btn-sm btn-danger\" data-toggle=\"popover\" title=\"User Not Logged In\" data-content=\"You are not logged in. Please Login to add the song to your playlist.\">+</button>\r\n" + 
-        			"\r\n" + 
-        			"                              <h5 class=\"card-title\">Song Name</h5>\r\n" + 
-        			"                              <p class=\"card-text\">Song Artist</p>\r\n" + 
-        			"                              <a href=\"#\" class=\"btn btn-primary\">Listen</a>\r\n" + 
-        			"                              <button type=\"button\" class=\"btn btn-sm btn-danger\" data-toggle=\"popover\" title=\"User Not Logged In\" data-content=\"You are not logged in. Please Login to add the song to your playlist.\">+</button>\r\n" + 
-        			"                            </div>\r\n" + 
-        			"                          </div>\r\n" + 
-        			"                        </div>\r\n" + 
-        			"                        <div class=\"col-sm-4\">\r\n" + 
-        			"\r\n" + 
-        			"                            <div style=\"background:transparent !important\" class=\"jumbotron\">\r\n" + 
-        			"                                <h1>Welcome to Website!</h1>\r\n" + 
-        			"                                <p>Blablabla, join us now and start making your slick playlist today!.\r\n" + 
-        			"                                  (Might make this a slideshow featuring some updates and other various things.)\r\n" + 
-        			"                                </p>\r\n" + 
-        			"                                <a class=\"btn btn-primary\">Register</a>\r\n" + 
-        			"                            </div>\r\n" + 
-        			"\r\n" + 
-        			"\r\n" + 
-        			"                        </div>\r\n" + 
-        			"                        <div class=\"col-sm-4\">Test3</div>\r\n" + 
-        			"                      </div>\r\n" + 
-        			"\r\n" + 
-        			"                   \r\n" + 
-        			"                          </div>\r\n" + 
-        			"\r\n" + 
-        			"                          \r\n" + 
-        			"\r\n" + 
-        			"                     \r\n" + 
-        			"\r\n" + 
-        			"\r\n" + 
-        			"\r\n" + 
-        			"        </body>\r\n" + 
-        			"\r\n" + 
-        			"        <!-- Footer -->\r\n" + PageElements.footer2()+
+        			"    </div>"+
+        			"        <!-- Footer -->\r\n" + PageElements.scripts()+ PageElements.footer2()+
         	
         			"  <!-- Footer -->\r\n" + 
         			"\r\n" + 
--- src/views/Test.java
+++ src/views/Test.java
@@ -1,5 +1,6 @@
 package views;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -25,225 +26,57 @@ public class Test extends DynamicWebPage
 	{
         if(toProcess.path.equalsIgnoreCase("aboutus.html"))
         {
-			String song;
-			song = toProcess.params.get("song").toLowerCase();
-			Song isong = new Song();
-			Artist iartist = new Artist();
-			Artist tempartist = new Artist();
-			MVMap<String, Song> songs= db.s.openMap("Song");
-			List<String> songkeys = songs.keyList() ;
-			MVMap<String, Artist> artists= db.s.openMap("Artist");
-			List<String> artistkeys = artists.keyList() ;
-			for(int i = 0; i<songkeys.size();i++) {
-				String songUniqueID = songkeys.get(i);
-				if(song.equalsIgnoreCase(songUniqueID)) {
-					isong = songs.get(songUniqueID);
-				}
-			}
-			//start of artist loop        
-			String searchresult = "";
-			for(int k = 0; k < artistkeys.size();k++) {	
-				String artistUniqueID = artistkeys.get(k);
-				tempartist = artists.get(artistUniqueID);
-				if(tempartist.artistSongs != null) {
-					for(int j = 0; j < tempartist.artistSongs.size() ;j++) {
-						String artistSong = tempartist.artistSongs.get(j);
-						if(artistSong.equalsIgnoreCase(isong.uniqueID.toLowerCase())) {
-							iartist = artists.get(artistUniqueID);
-						}
-					}
-				}
-			}		
+        	ArrayList<String> pageimages = new ArrayList<String>();
+        	pageimages.add("images/homepage/1.gif");
+        	pageimages.add("images/homepage/2.gif");
+        	pageimages.add("images/homepage/3.gif");
+        	pageimages.add("images/homepage/4.gif");
+        	pageimages.add("images/homepage/5.gif");
+        	pageimages.add("images/homepage/6.gif");//( max - min )) + min 
+        	Random rn = new Random();
+        	int randomimage = rn.nextInt(pageimages.size());
+        	String stringToSendToWebBrowser = PageElements.header()+PageElements.Navbar()+PageElements.Search()+
+        			"    <div class=\"row\">\r\n" + 
+        			"\r\n" + 
+        			"      <div class=\"col s12 m6 l6\"> <!Right Hand Side Panel-->\r\n" + 
+        			"<img class=\"materialboxed\" width=\"100%\" src=\""+pageimages.get(randomimage)+"\">"+
+        			"   </div>\r\n" + 
+        			"\r\n" + 
+        			"      <div class=\"col s12 m6 l6\"> <!-- Left Hand Panel -->\r\n" + 
+        			"  <div class=\"carousel carousel-slider center \">\r\n" + 
+        			"    <div class=\"carousel-item orange white-text\" href=\"#one!\">\r\n" + 
+        			"      <h2>First Panel</h2>\r\n" + 
+        			"      <p class=\"white-text\">This is your first panel</p>\r\n" + 
+        			"    </div>\r\n" + 
+        			"    <div class=\"carousel-item white black-text\" href=\"#two!\">\r\n" + 
+        			"      <h2>Second Panel</h2>\r\n" + 
+        			"      <p class=\"white-text\">This is your second panel</p>\r\n" + 
+        			"    </div>\r\n" + 
+        			"    <div class=\"carousel-item orange white-text\" href=\"#three!\">\r\n" + 
+        			"      <h2>Third Panel</h2>\r\n" + 
+        			"      <p class=\"white-text\">This is your third panel</p>\r\n" + 
+        			"    </div>\r\n" + 
+        			"    <div class=\"carousel-item white black-text\" href=\"#four!\">\r\n" + 
+        			"      <h2>Fourth Panel</h2>\r\n" + 
+        			"      <p class=\"white-text\">This is your fourth panel</p>\r\n" + 
+        			"    </div>\r\n" + 
+        			"  </div>\r\n" + 
+        			"        "+
+        			"      </div>\r\n" + 
+        			"\r\n" + 
+        			"    </div>"+
+        			"        <!-- Footer -->\r\n" + PageElements.scripts()+ PageElements.footer2()+
+        	
+        			"  <!-- Footer -->\r\n" + 
+        			"\r\n" + 
+        			"        \r\n" + 
+        			"    </head>\r\n" + 
+        			"</html>";
+        	
+        	
+        	toProcess.r = new WebResponse( WebResponse.HTTP_OK, WebResponse.MIME_HTML, stringToSendToWebBrowser );
 
-			// end of artist loop 
-			//start of mood loop   
-			MVMap<String, Mood> moods= db.s.openMap("Mood");
-			List<String> moodkeys = moods.keyList() ;
-			Mood imood = new Mood();
-			Mood tempmood = new Mood();
-			String moodresult = "";
-			for(int k = 0; k < moodkeys.size();k++) {	
-				String moodUniqueID = moodkeys.get(k);
-				tempmood = moods.get(moodUniqueID);
-				if(tempmood.songID != null) {
-					for(int j = 0; j < tempmood.songID.size() ;j++) {
-						String moodname = tempmood.songID.get(j);
-						if(moodname.equalsIgnoreCase(isong.uniqueID.toLowerCase())) {
-							imood = moods.get(moodUniqueID);
-						}
-					}
-				}
-			}		
-
-			// end of mood loop 
-			String videoString = "";
-			String thissong = isong.uniqueID;
-			String songtitle = isong.songtitle;
-			if(isong.songLink == null) {
-				videoString += "                  <p class=\"lead\" style=\"\">" + "video: no youtube video <br></p>\n" ; 
-			}else {
-					String[] youtubeparts =isong.songLink.split("=");
-					String youtubevideono = youtubeparts[1].trim();
-					videoString +="			<div class=\"video-container\">\r\n" + 
-									"			<iframe src=\"http://www.youtube.com/embed/"+ youtubevideono.trim() +"\"></iframe>"+
-									"      </div>\r\n" + 
-									"        ";	
-			}	
-
-			String stringToSendToWebBrowser = PageElements.header() + PageElements.Navbar()+ PageElements.Search();
-					
-			stringToSendToWebBrowser +=
-			"  <div id=\"index-banner\" class=\"parallax-container\">\r\n" + 
-			"    <div class=\"section no-pad-bot\">\r\n" + 
-			"      <div class=\"container\">\r\n" + 
-			"        <br><br>\r\n" + 
-			"        <h1 class=\"header center white-text text-lighten-2\"><b>Song: "+isong.songtitle+"</b></h1>\r\n" + 
-			"        <h2 class=\"header center white-text text-lighten-2\"><b>Artist:"+iartist.artistName+"</b></h2>\r\n" + 
-			"        <div class=\"row center\">\r\n" + 
-			"        </div>\r\n" + 
-			"        <div class=\"row center\">\r\n" + 
-			"        </div>\r\n" + 
-			"        <br><br>\r\n" + 
-			"\r\n" + 
-			"      </div>\r\n" + 
-			"    </div>\r\n" ;
-			if(iartist.artistImage != null && iartist.artistImage != "" ) {
-			stringToSendToWebBrowser +=	"    <div class=\"parallax\"><img src=\""+isong.songImage+"\" alt=\"Artist Image\"></div>\r\n" +
-							"  </div> ";
-			}else {
-			stringToSendToWebBrowser +=	"    <div class=\"parallax\"><img src=\"https://static.pingendo.com/img-placeholder-3.svg\" alt=\"Artist Image\"></div>\r\n" +
-							"  </div> ";			
-			}
-			stringToSendToWebBrowser+=	"  <div class=\"container\">\r\n" + 
-		"    <div class=\"section\">\r\n" + 
-			"\r\n" + 
-			"      <div class=\"row\">\r\n" + 
-			"        <div class=\"col s12 center\">\r\n" + 
-			"          <h3><i class=\"mdi-content-send black-text\"></i></h3>\r\n" + 
-			"          <h4>Song Information</h4><br>\r\n" + 
-			"          <p class=\"left-align light\">Song title: "+isong.songtitle+"</p>\r\n" + 
-			"          <a href = \"../artistpage.html?artist="+iartist.uniqueID+"\"><p class=\"left-align blue-text text-darken-2\">Artist: "+iartist.artistName+"</p></a>\r\n" + 
-			"          <p class=\"left-align light\">Song Length: "+isong.songlength+"</p>\r\n" + 
-			"          <a href = \"../moodpage.html?mood="+imood.moodname+"\"><p class=\"left-align blue-text text-darken-2\">Mood: "+imood.moodname+"</p></a>\r\n" + 
-			"        </div>\r\n" + 
-			"      </div>\r\n" + 
-			"\r\n" + 
-			"    </div>\r\n" + 
-			"  </div>";    
-			stringToSendToWebBrowser += "  <div class=\"container\">\r\n"+
-			"         		<h2>"+songtitle+" Music Video</h2>\n"+videoString;
-			if (imood.songID == null || imood.songID.size()<=1) {
-			stringToSendToWebBrowser += 	"  <div class=\"container\">\r\n" + 
-						"    <div class=\"section\">\r\n" + 
-						"\r\n" + 
-						"      <div class=\"row\">\r\n" + 
-						"        <div class=\"col s12 center\">\r\n" + 
-						"          <h3><i class=\"mdi-content-send black-text\"></i></h3>\r\n" + 
-						"          <h4>You Might also Like</h4><br>\r\n" + 
-						"          <a href = \"song.html\"<h4 class=\"left-align blue-text text-darken-2\">We Dont have any similar songs,click here to add one?</h4></a>\r\n" + 
-						"        </div>\r\n" + 
-						"      </div>\r\n" + 
-						"\r\n" + 
-						"    </div>\r\n" + 
-						"  </div>";    	
-			}else {
-				
-			stringToSendToWebBrowser +=	"  <div class=\"container\">\r\n" + 
-					"    <div class=\"section\">\r\n" + 
-					"\r\n" + 
-					"      <div class=\"row\">\r\n" + 
-					"        <div class=\"col s12 center\">\r\n" + 
-					"          <h3><i class=\"mdi-content-send black-text\"></i></h3>\r\n" + 
-					"          <h4>You Might also Like</h4><br>\r\n" + 
-					"        </div>\r\n" + 
-					"      </div>\r\n" + 
-					"\r\n" + 
-					"    </div>\r\n" + 
-					"  </div>\r\n"; 
-			//stringToSendToWebBrowser +=	"  <div class=\"container\">\r\n";
-			
-			if(imood.songID.size()<4) {
-				for(int i = 0; i < imood.songID.size();i++) {  
-					String songId = imood.songID.get(i);
-					isong = songs.get(songId);
-					String songName = isong.uniqueID;
-					
-					for(int j = 0; j < iartist.artistSongs.size() ;j++) {
-					String artistSong = iartist.artistSongs.get(j);
-						if(songName.equalsIgnoreCase(artistSong) && !songName.equalsIgnoreCase(thissong)) {
-						
-						stringToSendToWebBrowser +=	"      <div class=\"row\">\r\n" +
-													"    <div class=\"col s10 offset-s1 m6 l4\">\r\n" + 
-													"      <div class=\"card small\">\r\n" + 
-													"        <div class=\"card-image\">\r\n" + 
-													"          <img src=\""+isong.songImage+"\">\r\n" + 
-													"          <span class=\"card-title\">Same Mood</span>\r\n" + 
-													"        </div>\r\n" + 
-													"        <div class=\"card-content\">\r\n" + 
-													"          <p class = \"flow-text\">"+isong.songtitle+"</p>\r\n" + 
-													"        </div>\r\n" + 
-													"        <div class=\"card-action\">\r\n" + 
-													"          <a href=\"../songpage?song="+isong.uniqueID+"\">click to View</a>\r\n" + 
-													"        </div>\r\n" + 
-													"      </div>\r\n" + 
-													"    </div>";
-						
-						
-						}
-					}
-				}
-			}else {
-				for(int i = 0; i < 4 ;i++) {  
-					int r = (int) ((Math.random()*imood.songID.size())+ 1);
-					String songId = imood.songID.get(r);
-					isong = songs.get(songId);
-					String songName = isong.uniqueID;
-					
-					for(int j = 0; j < iartist.artistSongs.size() ;j++) {
-					String artistSong = iartist.artistSongs.get(j);
-						if(songName.equalsIgnoreCase(artistSong) && !songName.equalsIgnoreCase(thissong)) {
-						
-						stringToSendToWebBrowser +=	"      <div class=\"row\">\r\n" +
-													"    <div class=\"col s10 offset-s1 m6 l4\">\r\n" + 
-													"      <div class=\"card small\">\r\n" + 
-													"        <div class=\"card-image\">\r\n" + 
-													"          <img src=\""+isong.songImage+"\">\r\n" + 
-													"          <span class=\"card-title\">Same Mood</span>\r\n" + 
-													"        </div>\r\n" + 
-													"        <div class=\"card-content\">\r\n" + 
-													"          <p class = \"flow-text\">"+isong.songtitle+"</p>\r\n" + 
-													"        </div>\r\n" + 
-													"        <div class=\"card-action\">\r\n" + 
-													"          <a href=\"../songpage?song="+isong.uniqueID+"\">click to View</a>\r\n" + 
-													"        </div>\r\n" + 
-													"      </div>\r\n" + 
-													"    </div>";
-						
-						
-						}
-					}
-				}
-			}
-			
-			stringToSendToWebBrowser +=			//	"    </div>\r\n" +
-								"   </div>\r\n" +
-								"   </div>\r\n" +
-								"   </div>\r\n" +
-								"  </div>";
-			
-			}
-			stringToSendToWebBrowser +=			"    </div>\r\n" +
-												"    </div>\r\n" +
-												"    </div>\r\n" +
-												"  </div>\n";
-			stringToSendToWebBrowser += PageElements.scripts()+ PageElements.footer2();
-			stringToSendToWebBrowser +=
-					"</body>\n" + 
-							"\n" + 
-							"</html>";
-			toProcess.r = new WebResponse( WebResponse.HTTP_OK, WebResponse.MIME_HTML, stringToSendToWebBrowser );
-			return true;
-			// page which displays artists    
+        	return true;
         }
         return false;
 	}
