35eb184ed0ad5f2a01dae22b5426b2f3f77078cf
Justin <Justin@192.168.1.14>
2019-03-17 13:42:06 +0000

    Small layout changes to home page

--- src/views/HomePage.java
+++ src/views/HomePage.java
@@ -110,6 +110,7 @@ public class HomePage extends DynamicWebPage
 			}
 			else {
 				iproduct.title = "There are no products";
+				iproduct.filePathToImage = "images/bag.JPG";
 				
 			}
 			
@@ -135,11 +136,11 @@ public class HomePage extends DynamicWebPage
         			"   <div>\r\n" + 
         			"    <h3 class=\"flow-text \">We picked out some songs and moods you might like, click on the tabs below to try them out.<br> Support the site by buying some of the fresh apparel from the store.</h3>\r\n" + 
         			"  </div>\r\n" + 
-        			" <ul id=\"tabs-swipe-demo swipeable\" class=\"tabs\">\r\n" + 
-        			"    <li class=\"tab col s3\"><a href=\"#test-swipe-1\">Mood</a></li>\r\n" + 
-        			"    <li class=\"tab col s3\"><a class=\"active\" href=\"#test-swipe-2\">Artist</a></li>\r\n" + 
+        			" <ul id=\"tabs\" class=\"tabs\">\r\n" + 
+        			"    <li class=\"tab col s3\"><a  class=\"active\" href=\"#test-swipe-1\">Mood</a></li>\r\n" + 
+        			"    <li class=\"tab col s3\"><a href=\"#test-swipe-2\">Artist</a></li>\r\n" + 
         			"    <li class=\"tab col s3\"><a href=\"#test-swipe-3\">Song</a></li>\r\n" + 
-        			"    <li class=\"tab col s3\"><a href=\"#test-swipe-4\">Product</a></li>\r\n" + 
+        			"    <li class=\"tab col s3\"><a href=\"#test-swipe-4\">Merch</a></li>\r\n" + 
         			"  </ul>\r\n" + 
         			//card 1
         			"  <div id=\"test-swipe-1\" class=\"col s12  black-text center-align\">"+
